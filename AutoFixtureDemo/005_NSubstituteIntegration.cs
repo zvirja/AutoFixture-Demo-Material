b13e4ec9cb24e11db34b6112696b85400f06166b
Alex Povar <zvirja1@gmail.com>
2017-12-27 15:16:48 +0200

    Add NSubstitute demo

---
+++ AutoFixtureDemo/005_NSubstituteIntegration.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+using AutoFixture.Xunit2;
+using NSubstitute;
+using Xunit;
+
+namespace AutoFixtureDemo
+{
+  public class NSubstituteIntegration
+  {
+    public class ValueSource
+    {
+      public string Value { get; set; }
+    }
+
+    public interface IValueRepository
+    {
+      ValueSource GetValue(string id);
+    }
+
+    #region NSubstitute integration
+
+    /* To demo:
+     * 1. Auto vs AutoConfigured 
+     * 2. Different input - different output
+     */
+    [Fact]
+    public void IntegrationDemo()
+    {
+      var fixture = new Fixture();
+      fixture.Customize(new AutoNSubstituteCustomization());
+      //fixture.Customize(new AutoConfiguredNSubstituteCustomization());
+
+      var repo = fixture.Create<IValueRepository>();
+
+      var valueSource = repo.GetValue("foo");
+      //Assert.NotNull(valueSource);
+    }
+
+    #endregion
+
+    #region NSubstitute + XUnit
+
+    public class AutoNSubstituteData : AutoDataAttribute
+    {
+      public AutoNSubstituteData():
+        base(() => new Fixture().Customize(new AutoConfiguredNSubstituteCustomization()))
+      {
+      }
+    }
+
+    /* To demo:
+     * 1. Frozen ValueSource
+     */
+    [Theory, AutoNSubstituteData]
+    public void DemoXUnitIntegration(IValueRepository repo)
+    {
+      var valueSource = repo.GetValue("foo");
+
+      Assert.NotNull(valueSource);
+    }
+
+
+    #endregion
+
+    #region Freeze recup with NSubstitute + XUnit
+
+    public interface ILogger
+    {
+      void Log(string msg);
+    }
+
+    public class SelfLoggingType
+    {
+      private readonly ILogger _logger;
+
+      public string StringValue { get; }
+      public int IntValue { get; }
+      public bool BoolValue { get; }
+
+      public SelfLoggingType(string stringValue, int intValue, bool boolValue, ILogger logger)
+      {
+        StringValue = stringValue;
+        IntValue = intValue;
+        BoolValue = boolValue;
+
+        _logger = logger;
+      }
+
+      public void LogStringValue() => _logger.Log(StringValue);
+      public void LogIntValue() => _logger.Log(IntValue.ToString());
+      public void LogByteValue() => _logger.Log(BoolValue.ToString());
+    }
+
+    [Theory, AutoNSubstituteData]
+    public void ShouldLogValidNumber([Frozen] ILogger logger, SelfLoggingType sut)
+    {
+      // Act
+      sut.LogIntValue();
+
+      // Assert
+      logger.Received().Log(sut.IntValue.ToString());
+    }
+
+    #endregion
+  }
+}
